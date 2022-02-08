179330148750004853f4968706ec427406839b5b
TeKki22 <loicjoly2002@gmail.com>
2022-02-08 20:51:48 +0100

    Ajout du code du bot

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ Client.on("messageCreate", message => {
             .addField("Caveau de Verre", "[ Strat et coffres cachés](https://www.youtube.com/watch?v=mfYnMykvjJM&t=496s)\n [Objets à récupérer](https://www.youtube.com/watch?v=PG9jkHmutao&list=PLTg8yNY-9NDP5FJUgzlG1ERnTneIfVrg-&index=17)", false)
             .addField("Crypte de la Pierre", "[ Strat](https://www.youtube.com/playlist?list=PLcm8uKhXeKKzcoXylwMjv_NYCZ_seXyCe)\n [Coffres cachés](https://www.youtube.com/watch?v=Wvip7EhHgf0)", false)
             .addField("Dernier Voeu", "[ Strat](https://youtube.com/playlist?list=PLcm8uKhXeKKzsSlgdNywPnxonzcGTIDOM)\n [Coffres cachés](https://www.youtube.com/watch?v=h4ceFuHEOzg)\n [Tous les voeux](https://www.next-stage.fr/2018/09/destiny-2-comment-activer-les-15-voeux-du-mur-des-souhaits.html)", false)
-            .addField("Jardin du Salut", "[ Strat](https://youtube.com/playlist?list=PLcm8uKhXeKKyzyG55p_Gg2BVFto7NWHAh)\n [Coffre cachés](https://youtube.com/playlist?list=PLcm8uKhXeKKzxms83Ij3UNX1CN5c1VRby)")
+            .addField("Jardin du Salut", "[ Strat](https://youtube.com/playlist?list=PLcm8uKhXeKKyzyG55p_Gg2BVFto7NWHAh)\n [Coffres cachés](https://youtube.com/playlist?list=PLcm8uKhXeKKzxms83Ij3UNX1CN5c1VRby)")
         message.channel.send({embeds: [embed]});
     }
 });
